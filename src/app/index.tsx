7fff02a828c60a374b2b965c76a9bf18bfe05264
Guilherme Carvalho <gui0307carvalho@gmail.com>
2024-02-08 03:28:14 -0300

    Update font loading and add loading component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,5 @@
 import { View, Text } from 'react-native';
-import {
-  Inter_400Regular,
-  Inter_500Medium,
-  Inter_600SemiBold,
-  Inter_700Bold,
-} from "@expo-google-fonts/inter"
 
 export default function Home() {
-  return (
-    <View className='bg-slate-900 flex-1 pt-8'>
-      <Text className="text-white text-2xl">EU AMO A MARIA MERNANDA</Text>
-    </View>
-  );
+  return <View className='bg-slate-900 flex-1 pt-8'></View>;
 }
